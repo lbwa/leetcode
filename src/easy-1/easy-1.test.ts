59902840f137a47baccc9b6025632c2e0aa25cc3
Bowen Liu <mr_lbw@outlook.com>
2020-05-13 21:48:36 +0800

    feat: easy 1

---
+++ src/easy-1/easy-1.test.ts
@@ -0,0 +1,7 @@
+import { twoSum } from './index'
+
+describe('easy 1', () => {
+  it('[1,2,3,4] -- 7', () => {
+    expect(twoSum([1, 2, 3, 4], 7)).toEqual([2, 3])
+  })
+})
---
+++ src/easy-1/index.ts
@@ -0,0 +1,23 @@
+export function twoSum(nums: number[], target: number) {
+  const map: Record<string, number> = {}
+  let i = nums.length
+  while (i--) {
+    const current = nums[i]
+    if (map[target - current] !== undefined) {
+      return [i, map[target - current]]
+    }
+    map[current] = i
+  }
+}
+
+// export function twoSum(nums: number[], target: number) {
+//   const map: Record<string, number> = {}
+//   return nums.reduce<number[]>((result, cur, index) => {
+//     if (map[target - cur] !== undefined) {
+//       return [map[target - cur], index]
+//     } else {
+//       map[cur] = index
+//     }
+//     return result
+//   }, [])
+// }
