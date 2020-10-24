cc11dd2b0b1fd9a737a8e3530b1b371204f09a2b
Liu Bowen <mr_lbw@outlook.com>
2020-10-25 01:02:47 +0800

    chore(medium113): how dfs works

--- src/medium-113/solution.go
+++ src/medium-113/solution.go
@@ -18,10 +18,16 @@ func pathSum(root *node, sum int) (answer [][]int) {
 		if root.Left == nil && root.Right == nil && sum == 0 {
 			answer = append(answer, append([]int(nil), path...))
 		}
+		// 深入 root 的左子树，进而通过下一层检查来检查是否存在符合要求的路径
 		dfs(root.Left, sum)
+		// 深入 root 的右子树，进而通过下一层检查来检查是否存在符合要求的路径
 		dfs(root.Right, sum)
+		/*
+			移除 root 本身，因为包含 root 的路径已经在上述代码中遍历完成，故 **针对递归栈中** 之后的遍历得到路径中无 root 节点
+		*/
 		path = path[:len(path)-1]
 	}
+	// 前序遍历
 	dfs(root, sum)
 	return
 }
