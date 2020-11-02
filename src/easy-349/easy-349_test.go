00224d88cf543927069a132f4975f7d4caa31f74
Liu Bowen <mr_lbw@outlook.com>
2020-11-02 23:51:02 +0800

    feat(easy-349): intersection of two arrays

--- src/easy-349/easy-349_test.go
+++ src/easy-349/easy-349_test.go
@@ -1,19 +1,12 @@
 package easy349
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSolution(t *testing.T) {
-	got1 := TwoPointers([]int{1, 2, 2, 1}, []int{2, 2})
-	want1 := []int{2}
-	got2 := TwoPointers([]int{4, 9, 5}, []int{9, 4, 9, 8, 4})
-	want2 := []int{4, 9}
-	if !reflect.DeepEqual(got1, want1) {
-		t.Errorf(`%d - %d`, got1, want1)
-	}
-	if !reflect.DeepEqual(got2, want2) {
-		t.Errorf(`%d - %d`, got2, want2)
-	}
+	assert := assert.New(t)
+	assert.Equal([]int{2}, intersection([]int{1, 2, 2, 1}, []int{2, 2}))
 }
--- src/easy-349/solution.go
+++ src/easy-349/solution.go
@@ -4,37 +4,38 @@ import (
 	"sort"
 )
 
-// TwoPointers for resolving easy 349
-func TwoPointers(nums1, nums2 []int) []int {
+/*
+	左右指针移动的依据是： 在 **有序** 序列中，当 a[i] > b[j] 时，移动 b 序列的 j 指针，以使得 b[j] 的值尽可能靠近 a[i] 的值，进而才会有 a[i] == b[j] 的可能性，进而得到交集
+*/
+func intersection(nums1 []int, nums2 []int) (ans []int) {
 	sort.Ints(nums1)
 	sort.Ints(nums2)
-	length1, length2 := len(nums1), len(nums2)
+	len1, len2 := len(nums1), len(nums2)
 	i, j := 0, 0
-	answer := []int{}
 
-	for i < length1 && j < length2 {
-		if nums1[i] == nums2[j] {
-			if !has(answer, nums1[i]) {
-				answer = append(answer, nums1[i])
+	var isIncluded func([]int, int) bool
+	isIncluded = func(nums []int, num int) bool {
+		for _, val := range nums {
+			if val == num {
+				return true
+			}
+		}
+		return false
+	}
+
+	for i < len1 && j < len2 {
+		current1, current2 := nums1[i], nums2[j]
+		if current1 == current2 {
+			if !isIncluded(ans, current1) {
+				ans = append(ans, current1)
 			}
 			i++
 			j++
-		} else if nums1[i] > nums2[j] {
+		} else if current1 > current2 {
 			j++
-		} else if nums1[i] < nums2[j] {
+		} else {
 			i++
 		}
 	}
-
-	return answer
-}
-
-func has(nums []int, val int) bool {
-	for _, v := range nums {
-		if val == v {
-			return true
-		}
-	}
-
-	return false
+	return ans
 }
