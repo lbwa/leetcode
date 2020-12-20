51e6e004310f5e66fcf54c720926369e0301212a
Liu Bowen <mr_lbw@outlook.com>
2020-12-20 18:59:33 +0800

    test(easy206): fix test case

--- src/easy-206/easy-206_test.go
+++ src/easy-206/easy-206_test.go
@@ -8,8 +8,7 @@ import (
 
 func TestSolution(t *testing.T) {
 	assert := assert.New(t)
-	head := &node{Val: 1, Next: &node{Val: 2, Next: &node{Val: 3, Next: &node{Val: 4}}}}
 	want := &node{Val: 4, Next: &node{Val: 3, Next: &node{Val: 2, Next: &node{Val: 1}}}}
-	assert.Equal(want, reverseListIteration(head))
-	assert.Equal(want, reverseListRecursion(head))
+	assert.Equal(want, reverseListIteration(&node{Val: 1, Next: &node{Val: 2, Next: &node{Val: 3, Next: &node{Val: 4}}}}))
+	assert.Equal(want, reverseListRecursion(&node{Val: 1, Next: &node{Val: 2, Next: &node{Val: 3, Next: &node{Val: 4}}}}))
 }
