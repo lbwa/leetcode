c4f923fc8064580975251dee1e799baa85fb071c
Liu Bowen <mr_lbw@outlook.com>
2021-04-19 23:46:29 +0800

    fix(easy14): correct loop limitaion in golang
    resolve #4

--- src/easy-14/easy-14_test.go
+++ src/easy-14/easy-14_test.go
@@ -10,4 +10,5 @@ func TestSolution(t *testing.T) {
 	assert := assert.New(t)
 	assert.Equal("fl", longestCommonPrefix([]string{`flower`, `flow`, `flight`}))
 	assert.Equal("", longestCommonPrefix([]string{`dog`, `racecar`, `car`}))
+	assert.Equal("ab", longestCommonPrefix([]string{`abb`, `abc`}))
 }
--- src/easy-14/solution.go
+++ src/easy-14/solution.go
@@ -5,7 +5,7 @@ func longestCommonPrefix(strs []string) string {
 		return ``
 	}
 
-	for i := 0; i < len(strs); i++ {
+	for i := 0; i < len(strs[0]); i++ {
 		for j := 1; j < len(strs); j++ {
 			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
 				return strs[0][:i]
