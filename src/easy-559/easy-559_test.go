c855b0f62d76235d2fed2537eda8ded2ed2e2d7b
Liu Bowen <mr_lbw@outlook.com>
2021-06-10 00:12:32 +0800

    chore: code style

--- src/easy-559/easy-559_test.go
+++ src/easy-559/easy-559_test.go
@@ -11,17 +11,17 @@ func TestSolution(t *testing.T) {
 	assert.Equal(3, maxDepthBfs(&node{
 		Val: 1,
 		Children: []*node{
-			&node{Val: 3, Children: []*node{&node{Val: 5}, &node{Val: 6}}},
-			&node{Val: 2},
-			&node{Val: 4},
+			{Val: 3, Children: []*node{{Val: 5}, {Val: 6}}},
+			{Val: 2},
+			{Val: 4},
 		},
 	}))
 	assert.Equal(3, maxDepthDfs(&node{
 		Val: 1,
 		Children: []*node{
-			&node{Val: 3, Children: []*node{&node{Val: 5}, &node{Val: 6}}},
-			&node{Val: 2},
-			&node{Val: 4},
+			{Val: 3, Children: []*node{{Val: 5}, {Val: 6}}},
+			{Val: 2},
+			{Val: 4},
 		},
 	}))
 }
