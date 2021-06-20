6c68389a41471993f9f6c14404e92ebb3799fbc1
Liu Bowen <mr_lbw@outlook.com>
2021-06-20 16:23:36 +0800

    feat(medium-33: new solution with comments

--- src/medium-33/medium-33_test.go
+++ src/medium-33/medium-33_test.go
@@ -15,4 +15,11 @@ func TestSolution(t *testing.T) {
 	expect(t, search([]int{4, 5, 6, 7, 0, 1, 2}, 7), 3)
 	expect(t, search([]int{4, 5, 6, 7, 0, 1, 2}, 0), 4)
 	expect(t, search([]int{4, 5, 6, 7, 0, 1, 2}, 3), -1)
+
+	expect(t, search0([]int{4, 5, 6, 7, 0, 1, 2}, 4), 0)
+	expect(t, search0([]int{4, 5, 6, 7, 0, 1, 2}, 5), 1)
+	expect(t, search0([]int{4, 5, 6, 7, 0, 1, 2}, 6), 2)
+	expect(t, search0([]int{4, 5, 6, 7, 0, 1, 2}, 7), 3)
+	expect(t, search0([]int{4, 5, 6, 7, 0, 1, 2}, 0), 4)
+	expect(t, search0([]int{4, 5, 6, 7, 0, 1, 2}, 3), -1)
 }
--- src/medium-33/solution.go
+++ src/medium-33/solution.go
@@ -1,33 +1,81 @@
 package medium33
 
 func search(nums []int, target int) int {
-	if len(nums) < 1 {
-		return -1
-	}
-	low, high := 0, len(nums)-1
+	left, right := 0, len(nums)-1
 
-	for low <= high {
-		mid := low + (high-low)/2
-		if nums[mid] == target {
-			return mid
-		}
-		// 中点在旋转数组前半部分递增序列
-		if nums[low] <= nums[mid] {
-			// target 值在 low ~ mid 之间
-			if nums[low] <= target && target < nums[mid] {
-				high = mid - 1
+	// 参考 https://leetcode-cn.com/problems/search-in-rotated-sorted-array/solution/er-fen-fa-python-dai-ma-java-dai-ma-by-liweiwei141/
+	for left < right {
+		// 与题无关，当有边界设置为 left = mid 和 right = mid-1 出现时，需要将 mid 的下
+		// 取整调整为上取整，以避免出现死循环，故有 left + ((right-left+1) >> 1)
+		// 原因在于如果我们不+1，则是取下整，得到的中点 mid 即为 left，而在
+		// [left,right] 区间中只有两个元素时，若再次进入 if/else 分支，由于是闭区间，又会
+		// 取 mid 的值并赋给 left，从而使二者变为定值，便进入死循环。
+		mid := left + ((right - left + 1) >> 1)
+		pivot := nums[mid]
+
+		// step1: 首先判断 mid 所在的单调区间，利用单调性进行二分操作
+		//        数组最大值
+		//           /|
+		//          / |
+		//         /  |
+		// 数组拐点--- | ---数组拐点
+		//            |   /
+		//            |  /
+		//            | /
+		//            |/
+		//       数组中最小值
+		// mid 位于最小值到拐点区间，此时 [mid, right] 一定是有序的
+		if pivot < nums[right] {
+			if pivot <= target && target <= nums[right] {
+				left = mid // 下一轮区间 [mid, right]
 			} else {
-				low = mid + 1
+				right = mid - 1 // 下一轮区间 [left, mid-1]
 			}
-			// 中点在旋转数组后半部分递增序列
 		} else {
-			// target 在 mid ~ high 之间
-			if nums[mid] < target && target <= nums[high] {
-				low = mid + 1
+			// 结合 !(pivot < nums[right]) 条件，mid 此时位于拐点到最大值区间，此时 [left, mid] 一定是有序的。
+			if nums[left] <= target && target < pivot {
+				right = mid - 1 // 下一轮区间 [left, mid-1]
 			} else {
-				high = mid - 1
+				left = mid // 下一轮区间 [mid, right]
 			}
 		}
 	}
+
+	if nums[left] == target {
+		return left
+	}
+	return -1
+}
+
+func search0(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left < right {
+		mid := left + ((right - left) >> 1)
+
+		// mid 位于最小值到拐点区间，此时 [mid, right] 一定是有序的
+		if nums[mid] < nums[right] {
+			// 此 if 中保证始终可能有 target 等于 right 边界的比较
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid
+			}
+		} else {
+			// 结合 !(pivot < nums[right]) 条件，mid 此时位于拐点到最大值区间，此时 [left, mid] 一定是有序的。
+			// 此 if 中保证始终可能有 target 等于 left 边界的比较
+			// 此处有 target <= nums[mid] 判断是因为之前的 if 判断没有覆盖到等于 nums[mid] 的场景
+			if nums[left] <= target && target <= nums[mid] {
+				right = mid
+			} else {
+				left = mid + 1
+			}
+		}
+	}
+
+	if nums[left] == target {
+		return left
+	}
+
 	return -1
 }
